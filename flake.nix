4590649ef238e3b552ced94c146f2906b30c82eb
blockdoth <pepijn.pve@gmail.com>
2024-12-26 19:46:42 +0100

    nixfmt

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  outputs = inputs:
+  outputs =
+    inputs:
     let
       system = "x86_64-linux";
       pkgs = inputs.nixpkgs.legacyPackages.${system};
       lightstreamer-client-lib = pkgs.python3Packages.buildPythonPackage rec {
         pname = "lightstreamer_client_lib";
         version = "2.2.0";
-        propagatedBuildInputs = with pkgs.python3Packages; [ aiohttp jsonpatch ];
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          aiohttp
+          jsonpatch
+        ];
         src = pkgs.fetchPypi {
           inherit pname version;
           sha256 = "7306e42707ebc40144879854e0aa568383f37e1e006e5430f96455867761a73c";
@@ -16,21 +20,18 @@
     in
     {
       packages.${system} = {
-        iss-piss-stream = pkgs.writers.writePython3Bin "iss-piss-stream"{ 
-          libraries = [ lightstreamer-client-lib ]; 
+        iss-piss-stream = pkgs.writers.writePython3Bin "iss-piss-stream" {
+          libraries = [ lightstreamer-client-lib ];
           doCheck = false;
-          }
-          (builtins.readFile ./main.py);
+        } (builtins.readFile ./main.py);
       };
 
-    
       devShell.x86_64-linux = pkgs.mkShell {
         packages = with pkgs; [
-          lightstreamer-client-lib 
-          python3
+          lightstreamer-client-lib
           python3Packages.matplotlib
-        ];      
+          python3
+        ];
       };
     };
 }
-
